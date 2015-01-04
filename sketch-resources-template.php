302e8a0fd26caeeda9c7a31170b30606d31378d4
denzildoyle <yell@denzildoyle.me>
2015-01-04 02:06:05 -0400

    Add sketch resources page

---
+++ sketch-resources-template.php
@@ -0,0 +1,159 @@
+<?php // Template Name: Sketch Resources ?>
+
+<?php get_header(); ?>
+    <header class="sub-page">
+        <h1><?php echo get_the_title(); ?></h1>
+    </header>
+	<div class="content-area container ">
+		<div class="col-md-12 page">
+			<div class="intro">
+				<h1>Learn Sketch</h1>
+				<p>A short list of some of my favorite sketch related resources.</p>		
+			</div>
+
+			<h2>Tutorials</h2>
+			
+			<a href="https://www.youtube.com/playlist?list=PLLnpHn493BHE6UIsdKYlS5zu-ZYvx22CS" title="design plus code">
+				<h3>Sketch 3 Tutorials</h3>
+				<span class="line-arrow">→ </span><p>Learning the basics of Sketch</p>
+			</a>
+
+			<a href="https://designcode.io/" title="Design plus code">
+				<h3>Design+Code</h3>
+				<span class="line-arrow">→ </span><p>Learn sketch and iOS design with Xcode while building a news reader application from scratch to the App store</p>
+			</a>
+
+			<a href="http://sketchdesign.io/" title="Sketch Design">
+				<h3>Sketch Design</h3>
+				<span class="line-arrow">→ </span><p>Learn to design an iPhone app with Sketch 3</p>
+			</a>
+
+			<a href="http://www.sketchcasts.net/" title="SketchCasts">
+				<h3>Sketch Casts</h3>
+				<span class="line-arrow">→ </span><p>A weekly screencast covering Sketch app</p>
+			</a>
+
+			<a href="http://sketchtalk.io/" title="Sketch Talk">
+				<h3>Sketch Talk</h3>
+				<span class="line-arrow">→ </span><p>A sketch forum.</p>
+			</a>
+
+			<a href="https://vimeo.com/106757336" title="Cubic Bezier Curves - Under the Hood">
+				<h3>Cubic Bezier Curves - Under the Hood</h3>
+				<span class="line-arrow">→ </span><p>Take a peek under the hood to see how computers draw cubic Bézier curves, as used in design and motion graphics programs.</p>
+			</a>
+			
+			<h2>Free Resources</h2>
+
+			<a href="http://www.sketchappsources.com/" title="Sketch App Sources">
+				<h3>Sketch App Sources</h3>
+				<span class="line-arrow">→ </span><p>The premiere source of free design resources for Sketch 3</p>
+			</a>
+
+			<a href="http://sketchapp.tv/" title="Sketch App TV">
+				<h3>Sketch App TV</h3>
+				<span class="line-arrow">→ </span><p>Free premium Sketch App files. One file a day.</p>
+			</a>
+
+			<h2>Reading Material</h2>
+
+			<a href="https://medium.com/@KounterB/making-the-switch-a5e73642c4c3" title="Making the switch">
+				<h3>Making the switch</h3>
+				<span class="line-arrow">→ </span><p>Comments and ideas about Sketch, its place in design, usage and future.</p>
+			</a>
+
+			<a href="https://medium.com/sketch-app/harnessing-vector-awesomeness-in-sketch-3c9621408138" title="Harnessing Vector Awesomeness in Sketch">
+				<h3>Harnessing Vector Awesomeness in Sketch</h3>
+				<span class="line-arrow">→ </span><p>Vector shapes are Sketch’s flagship feature. Learn how to master them.</p>
+			</a>
+
+			<a href="https://medium.com/@jm_denis/discovering-sketch-25545f6cb161" title="Discovering Sketch">
+				<h3>Discovering Sketch</h3>
+				<span class="line-arrow">→ </span><p>“the designer’s toolbox”</p>
+			</a>
+
+			<a href="https://medium.com/@jm_denis/what-is-new-in-sketch-3-4b92d8b25f3" title="What is new in Sketch 3">
+				<h3>What is new in Sketch 3</h3>
+				<span class="line-arrow">→ </span><p>Everything you asked for and more</p>
+			</a>
+			
+			<a href="https://medium.com/google-design/sketch-tutorial_01-b76271a095e3" title="Sketch Tutorial_01">
+				<h3>Sketch Tutorial_01</h3>
+				<span class="line-arrow">→ </span><p>Colorful switch</p>
+			</a>
+
+			<a href="https://medium.com/sketch-app/sketch-shortcuts-4babfb40545b" title="Sketch Shortcuts">
+				<h3>Sketch Shortcuts</h3>
+				<span class="line-arrow">→ </span><p>A list of Keyboard Shortcuts</p>
+			</a>
+	
+			<a href="https://medium.com/sketch-app/mastering-the-bezier-curve-in-sketch-4da8fdf0dbbb" title="Mastering the Bézier Curve in Sketch">
+				<h3>Mastering the Bézier Curve in Sketch</h3>
+				<span class="line-arrow">→ </span><p>Break through the confusion and start making beautifully curved vector shapes.</p>
+			</a>		
+			
+			<h2>People to follow</h2>
+
+			<a href="http://sketchtalk.io/" title="design plus code">
+				<h3>Sketch Talk</h3>
+				<span class="line-arrow">→ </span><p>A sketch forum.</p>
+			</a>
+
+			<a href="https://twitter.com/deannnrie" title="Dean Rie">
+				<h3>Dean Rie - @deannnrie</h3>
+				<span class="line-arrow">→ </span><p>web/iOS developer and UI/UX designer #sketchapp #swiftlang #nodejs #angularjs #golang</p>
+			</a>
+
+			<a href="https://twitter.com/Creativedash" title="Creativedash">
+				<h3>Creativedash - @Creativedash</h3>
+				<span class="line-arrow">→ </span><p>Creativeda.sh is a digital studio focused on high-end and exceptional creative projects.</p>
+			</a>
+
+			<a href="https://twitter.com/sketchappTV" title="sketchappTV">
+				<h3>sketchappTV - @sketchappTV</h3>
+				<span class="line-arrow">→ </span><p>Free Sketch 3 Premium Resources. One file a day. (Product Hunt '14)</p>
+			</a>
+
+			<a href="https://twitter.com/rafahari" title="sketchappTV">
+				<h3>Rafael Conde - @rafahari</h3>
+				<span class="line-arrow">→ </span><p>Designer at Imaginary Cloud Co-Founder of SketchCasts.</p>
+			</a>
+
+			<a href="https://twitter.com/sketchcasts" title="SketchCasts">
+				<h3>SketchCasts - @sketchcasts</h3>
+				<span class="line-arrow">→ </span><p>A weekly screencast covering Sketch</p>
+			</a>
+			
+			<a href="https://twitter.com/emanuelsa" title="Emanuel Sá">
+				<h3>Emanuel Sá - @emanuelsa</h3>
+				<span class="line-arrow">→ </span><p>The toolbox at @bohemiancoding</p>
+			</a>
+
+			<a href="https://twitter.com/sketchapphub" title="Sketch Talk">
+				<h3>Sketch Talk - @sketchapphub</h3>
+				<span class="line-arrow">→ </span><p>A community for Sketch users of all skill levels. Exchange your thoughts, links, tips and best practices with other @sketchapp designers and developers.</p>
+			</a>
+
+			<a href="https://twitter.com/philippehong" title="Philippe HONG">
+				<h3>Philippe HONG - @philippehong</h3>
+				<span class="line-arrow">→ </span><p>Art Director, front developer &amp; video lover. Doing Freelance. Co-Founder of @vytein. http://philippehong.com </p>
+			</a>
+
+			<a href="https://twitter.com/KounterB" title="Sebastien Gabriel">
+				<h3>Sebastien Gabriel - @KounterB</h3>
+				<span class="line-arrow">→ </span><p>Designer for @googlechrome. T-Rex breeder.</p>
+			</a>
+
+			<a href="https://twitter.com/jm_denis" title="Jean-Marc Denis">
+				<h3>Jean-Marc Denis - @jm_denis</h3>
+				<span class="line-arrow">→ </span><p>Mobile product lead @InboxByGmail @Google. Previously @Sparrow. http://sketchdesign.io  and http://just1.io  author.</p>
+			</a>
+
+			<a href="https://twitter.com/DenzilDoyle" title="Denzil Doyle" id="my-twitter-account">
+				<h3>Denzil Doyle - @DenzilDoyle</h3>
+				<span class="line-arrow">→ </span><p>Passionate problem solver. Entrepreneur. Web and mobile designer/developer. Podcast addict. Mad Scientist. Occasional comedian. Linkedin</p>
+			</a>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
