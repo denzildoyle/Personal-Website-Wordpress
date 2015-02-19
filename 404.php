4d232ad1c6e9442cd0f0d75afcac81c6c4c446e7
denzildoyle <yell@denzildoyle.me>
2015-02-19 00:47:17 -0400

    Add new temperary 404 page

--- 404.php
+++ 404.php
@@ -1,2 +1,26 @@
-<?php get_header(); ?>				
-	<center><h1><?php _e("PAGE NOT FOUND","wpbootstrap"); ?></h1></center>
+<?php get_header(); ?>	
+		<style>
+			.page-error{
+				text-align: center;
+				display: block;
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				margin-top: -200px;
+				margin-left: -174px;
+			}
+			.page-error h1{
+				font-size: 200px;
+			}
+			.page-error h3{
+				font-size: 24px;
+				text-transform: uppercase;
+			}
+
+		</style>
+		<div class="page-error">
+			<h1>404</h1>
+			<h3>page not found</h3>
+		</div>
+    </body>
+</html>
