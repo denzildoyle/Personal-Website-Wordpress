84ff97ae363f1e5e8e782ac9acf0cc327d2a7015
denzildoyle <yell@denzildoyle.me>
2015-03-22 11:18:07 -0400

    Remove code comments

--- index.php
+++ index.php
@@ -12,22 +12,6 @@
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate incidunt doloribus repellat quaerat nemo suscipit officia temporibus, molestiae nihil nisi sint tempore corporis optio provident? Qui natus facilis quas hic.</p>
                     </div>   
                 </div>
-
-
-<!--                 <div class="col-md-12 skills" id="skills">
-                    <div class="col-xs-4 clear">
-                        <img src="<?php bloginfo('template_url'); ?>/img/web.svg" class="center-block img-responsive">
-                        <h4>Web Development</h4>
-                    </div>
-                    <div class="col-xs-4 clear">
-                        <img src="<?php bloginfo('template_url'); ?>/img/mobile.svg" class="center-block img-responsive">
-                        <h4>Mobile Development</h4>
-                    </div>
-                    <div class="col-xs-4 clear">
-                        <img src="<?php bloginfo('template_url'); ?>/img/ui.svg" class="center-block img-responsive">
-                        <h4>UI Design</h4>
-                    </div>
-                </div> -->
             </div>
         </div>
     </header>
@@ -41,91 +25,60 @@
     </div>
   
     <div class="row clear">
-        <div class="col-md-6 clear">
-            <a href="http://www.pizzahut-tt.com/" id="ga-pizza-hut-img"><img src="<?php bloginfo('template_url'); ?>/img/projects/pizza-hut-tnt.jpg" class="center-block img-responsive" alt="pizza hut website" style="width: 100%; heigth: 100%;"></a>
+        <div class="col-md-6 clear grid">
+            <figure class="effect-lily">
+                <a href="http://www.pizzahut-tt.com/"><img src="<?php bloginfo('template_url'); ?>/img/projects/pizza-hut-tnt.jpg" class="center-block img-responsive" alt="pizza hut website"></a>
+                <figcaption>
+                    <div>
+                        <h2>Nice <span>Lily</span></h2>
+                        <p>Lily likes to play with crayons and pencils</p>
+                    </div>
+                    <a href="#">View more</a>
+                </figcaption>           
+            </figure>
         </div>
-        <div class="col-md-6 clear">
-            <a href="https://www.grahamtt.azurewebsites.net"><img src="<?php bloginfo('template_url'); ?>/img/projects/graham-trainer-website.jpg" class="center-block img-responsive" alt="graham the trainer website"></a>
-        </div>
-        <div class="col-md-6 clear">
-            <a href="http://locationscout.apsolute-tt.com/"><img src="<?php bloginfo('template_url'); ?>/img/projects/location-scout.jpg" class="center-block img-responsive" alt="location scout website"></a>
-        </div>
-<!--         <div class="col-md-6 clear">
-            <a href="http://locationscout.apsolute-tt.com/"><img src="<?php bloginfo('template_url'); ?>/img/projects/location-scout-mobile-app.png" class="center-block img-responsive" alt="location scout website"></a>
-        </div> -->
-        <div class="col-md-6 clear">
-            <a href="http://locationscout.apsolute-tt.com/"><img src="<?php bloginfo('template_url'); ?>/img/projects/usc-football-team.jpg" class="center-block img-responsive" alt="location scout website"></a>
+
+        <div class="col-md-6 clear grid">
+            <figure class="effect-lily">
+                <a href="https://www.grahamtt.azurewebsites.net"><img src="<?php bloginfo('template_url'); ?>/img/projects/graham-trainer-website.jpg" class="center-block img-responsive" alt="graham the trainer website"></a>
+                <figcaption>
+                    <div>
+                        <h2>Nice <span>Lily</span></h2>
+                        <p>Lily likes to play with crayons and pencils</p>
+                    </div>
+                    <a href="#">View more</a>
+                </figcaption>           
+            </figure>
         </div> 
-            <div class="col-md-6 clear grid">
-                    <figure class="effect-lily">
-                        <img src="<?php bloginfo('template_url'); ?>/img/projects/usc-football-team.jpg" class="center-block img-responsive" alt="location scout website">
-                        <figcaption>
-                            <div>
-                                <h2>Nice <span>Lily</span></h2>
-                                <p>Lily likes to play with crayons and pencils</p>
-                            </div>
-                            <a href="#">View more</a>
-                        </figcaption>           
-                    </figure>
-                </div>     
+            
+        <!-- <a href="http://locationscout.apsolute-tt.com/"><img src="<?php bloginfo('template_url'); ?>/img/projects/location-scout-mobile-app.png" class="center-block img-responsive" alt="location scout website"></a> -->
+        
+        <div class="col-md-6 clear grid">
+            <figure class="effect-lily">
+                <a href="http://locationscout.apsolute-tt.com/"><img src="<?php bloginfo('template_url'); ?>/img/projects/location-scout.jpg" class="center-block img-responsive" alt="location scout website"></a>
+                <figcaption>
+                    <div>
+                        <h2>Nice <span>Lily</span></h2>
+                        <p>Lily likes to play with crayons and pencils</p>
+                    </div>
+                    <a href="#">View more</a>
+                </figcaption>           
+            </figure>
+        </div> 
+
+        <div class="col-md-6 clear grid">
+            <figure class="effect-lily">
+                <img src="<?php bloginfo('template_url'); ?>/img/projects/usc-football-team.jpg" class="center-block img-responsive" alt="location scout website">
+                <figcaption>
+                    <div>
+                        <h2>Nice <span>Lily</span></h2>
+                        <p>Lily likes to play with crayons and pencils</p>
+                    </div>
+                    <a href="#">View more</a>
+                </figcaption>           
+            </figure>
+        </div>     
     </div>
-
-
-<!--         <div class="container-fluid project">
-            <div class="col-md-8 col-md-offset-2">
-                
-            </div>
-        </div>
-        <div class="social-section">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="description">
-                <p><b>Pizza Hut</b><br>
-                website redesign and development</p>
-                <p class="share-btns">
-                    <a href="https://twitter.com/share" data-related="denzildoyle: Web Developer" data-text="Entrepreneur, web designer/developer. Founder/CTO of @apsolute_tt Linkedin: goo.gl/5GBu7O" data-url="http://www.denzildoyle.me/" data-via="denzildoyle" class="twitter-share-button center-block" data-lang="en">Tweet</a>
-                    <div class="fb-share-button" data-href="http://www.denzildoyle.me/" data-type="button_count"></div>
-                </p>                
-                </div>
-            </div>
-        </div>
-
-        <div class="container-fluid project">
-            <div class="col-md-8 col-md-offset-2">
-                
-            </div>              
-        </div>
-        <div class="social-section">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="description">
-                <p><b>Graham The Trainer</b><br>
-                website design and development</p>
-                <p class="share-btns">
-                    <a href="https://twitter.com/share" data-related="denzildoyle: Web Developer" data-text="Entrepreneur, web designer/developer. Founder/CTO of @apsolute_tt Linkedin: goo.gl/5GBu7O" data-url="http://www.denzildoyle.me/" data-via="denzildoyle" class="twitter-share-button center-block" data-lang="en">Tweet</a>
-                    <div class="fb-share-button" data-href="http://www.denzildoyle.me/" data-type="button_count"></div>
-                </p>                
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-fluid project">
-            <div class="col-md-8 col-md-offset-2">
-                
-            </div>              
-        </div>
-        <div class="social-section">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="description">
-                <p><b>Location Scout</b><br>
-                website design and development</p>
-                <p class="share-btns">
-                    <a href="https://twitter.com/share" data-related="denzildoyle: Web Developer" data-text="Entrepreneur, web designer/developer. Founder/CTO of @apsolute_tt Linkedin: goo.gl/5GBu7O" data-url="http://www.denzildoyle.me/" data-via="denzildoyle" class="twitter-share-button center-block" data-lang="en">Tweet</a>
-                    <div class="fb-share-button" data-href="http://www.denzildoyle.me/" data-type="button_count"></div>
-                </p>                
-                </div>
-            </div>
-        </div>
-    </div> -->
 
     <div class="tweet-container container" ng-app="myApp" ng-controller="FeedCtrl">
         <div class="col-md-4 col-md-offset-4 col-xs-10 col-xs-offset-1 tweet">   
