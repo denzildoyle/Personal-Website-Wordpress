16070c9bfa9be022d191f4d53aff95a38067e253
denzildoyle <yell@denzildoyle.me>
2015-03-22 22:00:29 -0400

    Add work to home page with external links

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                     <div id="about-me" class="details">
                         <h1><em>UI/UX Designer, Developer</em></h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate incidunt doloribus repellat quaerat nemo suscipit officia temporibus, molestiae nihil nisi sint tempore corporis optio provident? Qui natus facilis quas hic.</p>
-                    </div>   
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,73 +20,69 @@
             <div class="col-md-4 col-md-offset-4">
                 <h1>Work</h1>
                 <p>Some of the work I have done both personally and professionally</p>
-            </div> 
+            </div>
         </div>
     </div>
-  
+
     <div class="row clear">
-        <div class="col-md-6 clear grid">
+        <a class="col-md-6 clear grid" href="http://www.pizzahut-tt.com/">
             <figure class="effect-lily">
-                <a href="http://www.pizzahut-tt.com/"><img src="<?php bloginfo('template_url'); ?>/img/projects/pizza-hut-tnt.jpg" class="center-block img-responsive" alt="pizza hut website"></a>
+                <img src="<?php bloginfo('template_url'); ?>/img/projects/pizza-hut-tnt.jpg" class="center-block img-responsive" alt="pizza hut website">
                 <figcaption>
                     <div>
-                        <h2>Nice <span>Lily</span></h2>
-                        <p>Lily likes to play with crayons and pencils</p>
+                        <h2>Pizza Hut</h2>
+                        <p>Impliment website redesign, while at <em>Caribbean Ideas</em> // Designed by <em>Trevyn R</em></p>
                     </div>
-                    <a href="#">View more</a>
-                </figcaption>           
+                </figcaption>
             </figure>
-        </div>
+        </a>
 
-        <div class="col-md-6 clear grid">
+        <a class="col-md-6 clear grid" href="https://www.grahamtt.azurewebsites.net">
             <figure class="effect-lily">
-                <a href="https://www.grahamtt.azurewebsites.net"><img src="<?php bloginfo('template_url'); ?>/img/projects/graham-trainer-website.jpg" class="center-block img-responsive" alt="graham the trainer website"></a>
+                <img src="<?php bloginfo('template_url'); ?>/img/projects/graham-trainer-website.jpg" class="center-block img-responsive" alt="graham the trainer website">
                 <figcaption>
                     <div>
-                        <h2>Nice <span>Lily</span></h2>
-                        <p>Lily likes to play with crayons and pencils</p>
+                        <h2>Graham the Trainer</h2>
+                        <p>Impliment website design by <em>Trevyn R</em></p>
                     </div>
-                    <a href="#">View more</a>
-                </figcaption>           
+                </figcaption>
             </figure>
-        </div> 
-            
+        </a>
+
         <!-- <a href="http://locationscout.apsolute-tt.com/"><img src="<?php bloginfo('template_url'); ?>/img/projects/location-scout-mobile-app.png" class="center-block img-responsive" alt="location scout website"></a> -->
-        
-        <div class="col-md-6 clear grid">
-            <figure class="effect-lily">
-                <a href="http://locationscout.apsolute-tt.com/"><img src="<?php bloginfo('template_url'); ?>/img/projects/location-scout.jpg" class="center-block img-responsive" alt="location scout website"></a>
-                <figcaption>
-                    <div>
-                        <h2>Nice <span>Lily</span></h2>
-                        <p>Lily likes to play with crayons and pencils</p>
-                    </div>
-                    <a href="#">View more</a>
-                </figcaption>           
-            </figure>
-        </div> 
 
-        <div class="col-md-6 clear grid">
+        <a class="col-md-6 clear grid" href="http://locationscout.apsolute-tt.com/">
             <figure class="effect-lily">
-                <img src="<?php bloginfo('template_url'); ?>/img/projects/usc-football-team.jpg" class="center-block img-responsive" alt="location scout website">
+                <img src="<?php bloginfo('template_url'); ?>/img/projects/location-scout.jpg" class="center-block img-responsive" alt="location scout website">
                 <figcaption>
                     <div>
-                        <h2>Nice <span>Lily</span></h2>
-                        <p>Lily likes to play with crayons and pencils</p>
+                        <h2>Location Scout</h2>
+                        <p>Windows phone 8 application designed &amp; developed</p>
                     </div>
-                    <a href="#">View more</a>
-                </figcaption>           
+                </figcaption>
             </figure>
-        </div>     
+        </a>
+
+        <a class="col-md-6 clear grid" href="https://www.behance.net/gallery/6045315/Lawson-Blacks-Foundation-Logo">
+            <figure class="effect-lily">
+                <img src="<?php bloginfo('template_url'); ?>/img/projects/usc-football-team.jpg" class="center-block img-responsive">
+                <figcaption>
+                    <div>
+                        <h2>USC Football</h2>
+                        <p>USC football team branding</p>
+                    </div>
+                </figcaption>
+            </figure>
+        </a>
     </div>
 
     <div class="tweet-container container" ng-app="myApp" ng-controller="FeedCtrl">
-        <div class="col-md-4 col-md-offset-4 col-xs-10 col-xs-offset-1 tweet">   
+        <div class="col-md-4 col-md-offset-4 col-xs-10 col-xs-offset-1 tweet">
             <hr>
             <p>{{tweet}}</p>
             <a href="https://twitter.com/DenzilDoyle" class="small">Follow me on Twitter</a>
             <hr>
-        </div>   
+        </div>
     </div>
 
     <div class="row clear">
