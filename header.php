3afb34cb6d2d737050b5175a64458a6983f981d4
denzildoyle <yell@denzildoyle.me>
2015-03-30 10:56:07 -0400

    Clean up leader removing unnecessary code

--- header.php
+++ header.php
@@ -4,10 +4,21 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui">
-        <meta name="author" content="Denzil Doyle">
+        <link rel="author" href="https://plus.google.com/u/0/+DenzilDoyle/posts"/>
 
-  		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-        <!-- <title><?php wp_title(''); ?>/title> -->
+        <meta name="viewport" content="width=device-width">
+
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="bingbot" content="index, follow" />
+        <meta name="yahoo-verticalcrawler" content="index, follow" />
+        <meta name="yahoo! slurp" content="index, follow" />
+        <meta name="msnbot" content="index, follow" />
+        <meta name="scooter" content="index, follow" />
+        <meta name="boitho.com-dc" content="index, follow" />
+        <meta name="sohu-search" content="index, follow" />
+
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
 		<!-- IE8 fallback moved below head to work properly. Added respond as well. Tested to work. -->
 			<!-- media-queries.js (fallback) -->
@@ -28,8 +39,6 @@
         <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/img/favicon.png" type="image/x-icon">
         <link rel="icon" href="<?php bloginfo('template_url'); ?>/img/favicon.png" type="image/x-icon">
 
-		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/small.css" media="(max-width: 400px)">
-
         <!-- CSS Files comes here -->
         <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css" media="screen">
 
@@ -39,8 +48,6 @@
         <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/fontawesome-ie7.css">
         <![endif]-->
         <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/animate.min.css" media="screen">
-        <!-- <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/lightbox.css" media="screen"> -->
-        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/prism.css" media="screen">
         <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" media="screen">
 		<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
 		<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
@@ -49,19 +56,6 @@
 		<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 
 		<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>img/favicon.png">
-
-	    <meta name="viewport" content="width=device-width">
-
-        <meta name="robots" content="index, follow" />
-        <meta name="googlebot" content="index, follow" />
-        <meta name="bingbot" content="index, follow" />
-        <meta name="yahoo-verticalcrawler" content="index, follow" />
-        <meta name="yahoo! slurp" content="index, follow" />
-        <meta name="msnbot" content="index, follow" />
-        <meta name="scooter" content="index, follow" />
-        <meta name="boitho.com-dc" content="index, follow" />
-        <meta name="sohu-search" content="index, follow" />
-
         <?php wp_head(); ?>
 	</head>
 
