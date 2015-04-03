b9357c58acd5124cc3c4cd483f2a7d13a50f1abc
denzildoyle <yell@denzildoyle.me>
2015-04-03 13:32:34 -0400

    Style blog post Archive page

--- archive.php
+++ archive.php
@@ -1,67 +1,52 @@
 <?php get_header(); ?>
-			
-			<div id="content" class="clearfix row">
-			
-				<div id="main" class="col-sm-8 clearfix" role="main">
-				
-					<div class="page-header">
-					<?php if (is_category()) { ?>
-						<h1 class="archive_title h2">
-							<span><?php _e("Posts Categorized:", "wpbootstrap"); ?></span> <?php single_cat_title(); ?>
-						</h1>
-					<?php } elseif (is_tag()) { ?> 
-						<h1 class="archive_title h2">
-							<span><?php _e("Posts Tagged:", "wpbootstrap"); ?></span> <?php single_tag_title(); ?>
-						</h1>
-					<?php } elseif (is_author()) { ?>
-						<h1 class="archive_title h2">
-							<span><?php _e("Posts By:", "wpbootstrap"); ?></span> <?php get_the_author_meta('display_name'); ?>
-						</h1>
-					<?php } elseif (is_day()) { ?>
-						<h1 class="archive_title h2">
-							<span><?php _e("Daily Archives:", "wpbootstrap"); ?></span> <?php the_time('l, F j, Y'); ?>
-						</h1>
-					<?php } elseif (is_month()) { ?>
-					    <h1 class="archive_title h2">
-					    	<span><?php _e("Monthly Archives:", "wpbootstrap"); ?></span> <?php the_time('F Y'); ?>
-					    </h1>
-					<?php } elseif (is_year()) { ?>
-					    <h1 class="archive_title h2">
-					    	<span><?php _e("Yearly Archives:", "wpbootstrap"); ?></span> <?php the_time('Y'); ?>
-					    </h1>
-					<?php } ?>
-					</div>
+	<header class="single-page" style="background-image: url('')">
+		<div class="layer">
+			<div class="title">
+	    		<div class="container">
+	    			<div class="col-md-12">
+	    				<?php if (is_category()) { ?>
+							<h1><?php _e("Category: ", "wpbootstrap"); ?><?php single_cat_title(); ?></h1>
+						<?php } elseif (is_tag()) { ?>
+							<h1><?php _e("Tag: ", "wpbootstrap"); ?><?php single_tag_title(); ?></h1>
+						<?php } elseif (is_author()) { ?>
+							<h1><?php _e("Posts By: ", "wpbootstrap"); ?><?php get_the_author_meta('display_name'); ?></h1>
+						<?php } elseif (is_day()) { ?>
+							<h1><?php _e("Daily Archives: ", "wpbootstrap"); ?><?php the_time('l, F j, Y'); ?></h1>
+						<?php } elseif (is_month()) { ?>
+						    <h1><?php _e("Monthly Archives: ", "wpbootstrap"); ?><?php the_time('F Y'); ?></h1>
+						<?php } elseif (is_year()) { ?>
+						    <h1><?php _e("Yearly Archives: ", "wpbootstrap"); ?><?php the_time('Y'); ?></h1>
+						<?php } ?>
+			    	</div>
+	    		</div>
+	    	</div>
+		</div>
+    </header>
 
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-						
-						<header>
-							
-							<h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-							
-							<p class="meta"><?php _e("Posted", "wpbootstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php _e("by", "wpbootstrap"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "wpbootstrap"); ?> <?php the_category(', '); ?>.</p>
-						
-						</header> <!-- end article header -->
-					
-						<section class="post_content">
-						
-							<?php the_post_thumbnail( 'wpbs-featured' ); ?>
-						
-							<?php the_excerpt(); ?>
-					
-						</section> <!-- end article section -->
-						
-						<footer>
-							
-						</footer> <!-- end article footer -->
-					
-					</article> <!-- end article -->
-					
-					<?php endwhile; ?>	
-					
-					<?php if (function_exists('wp_bootstrap_page_navi')) { // if expirimental feature is active ?>
-						
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div class="content-area container ">
+			<article  class="col-md-12" id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+				<!-- Title -->
+				<h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+				<!-- Author and tags -->
+				<p class="meta"><?php _e("Posted", "wpbootstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(); ?></time> <?php _e("by", "wpbootstrap"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "wpbootstrap"); ?> <?php the_category(', '); ?>.</p>
+
+				<!-- Fretured Image -->
+				<div class="row">
+					<div class="col-md-4">
+						<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+						<img src="<?php echo $image[0]; ?>" alt="" style="width:100%;">
+					</div>
+					<div class="col-md-8">
+						<?php the_excerpt(); ?>
+					</div>
+				</div>
+			</article>
+		</div>
+	<?php endwhile; ?>
+
+<!-- 					<?php if (function_exists('wp_bootstrap_page_navi')) { // if expirimental feature is active ?>
+
 						<?php wp_bootstrap_page_navi(); // use the page navi function ?>
 
 					<?php } else { // if it is disabled, display regular wp prev & next links ?>
@@ -72,10 +57,10 @@
 							</ul>
 						</nav>
 					<?php } ?>
-								
-					
+
+
 					<?php else : ?>
-					
+
 					<article id="post-not-found">
 					    <header>
 					    	<h1><?php _e("No Posts Yet", "wpbootstrap"); ?></h1>
@@ -86,13 +71,7 @@
 					    <footer>
 					    </footer>
 					</article>
-					
-					<?php endif; ?>
-			
-				</div> <!-- end #main -->
-    
-				<?php get_sidebar(); // sidebar 1 ?>
-    
-			</div> <!-- end #content -->
+
+					<?php endif; ?> -->
 
 <?php get_footer(); ?>
